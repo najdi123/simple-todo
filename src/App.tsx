6abbd319009222f5bc9a35cca7e33f9d91ff67f4
monire91 <aglida1370@gmail.com>
2025-07-23 08:15:54 -0500

    Add ToDo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,44 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+type TodoListType={id:string,title:string}[]
+
+const TodoList:TodoListType=[
+  {id:"1",title:"dished"},
+  {id:"2",title:"clean room"},
+  {id:"3",title:"tasks"},
+  {id:"4",title:"shower"},
+]
+
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [tasks,setTasks]=useState(TodoList)
+  const [newTask,setNewTask]=useState("")
+
+  const addTask=()=>{
+    setTasks((prev)=>[...prev,{id:Date.now()+"",title:newTask}])
+    setNewTask("")
+  }
+
+  const handleRemove=(id:string)=>{
+    const UpdatedTask=tasks.filter((item)=>item.id!==id)
+    setTasks(UpdatedTask)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="AddButton" onClick={addTask}>Add Task</button>
+        <input type="text" value={newTask} onChange={(e)=>setNewTask(e.target.value)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {tasks?.map((item)=>{
+        return(
+          <div className="TaskItem">
+            <p>{item.title}</p>
+            <button onClick={()=>handleRemove(item.id)}>Remove Task</button>
+          </div>
+        )
+      })}
     </>
   );
 }
